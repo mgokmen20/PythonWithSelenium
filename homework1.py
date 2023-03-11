9764b9e3dbcee352a19756be2de728d0cf1610bf
Mehmet Gökmen <96737900+mgokmen20@users.noreply.github.com>
2023-03-11 14:18:21 +0100

    Add files via upload
    Veri tipleri ve sart bloklari örnekleri

---
+++ homework1.py
@@ -0,0 +1,50 @@
+# Homework - 1
+
+
+# 1-)  Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazın
+
+# String Veri Tipi : Metinsel veri tipleri icin kullanilir . 
+greeting= "Hello World!"
+
+# Integer (int) Veri Tipi : Tam sayılar için kullanılır.     
+
+num = 20
+
+# Float Veri Tipi : Ondalikli sayilar icin kullanılır. 
+
+pi = 3.14
+
+# Boolean Veri Tipi : Mantiksal veri tiplerinde kullanilir. 
+
+value = True
+
+
+
+
+# 2-) Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+
+# Email= Kullanicilarin giris yaptiklari mail adresi string deger
+# Password = Kullanicilarin sifreleri - kullanicinin sifresinin tutuldugu string deger
+
+
+
+
+# 3-) Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+username = input("Kullanici adi giriniz : ")
+password = input("Sifre giriniz : ")
+
+
+
+
+if username == "mehmet":
+    if password == "1234":
+        print("Giris basarili")
+    else:
+        print("sifrenizi kontrol ediniz")
+else:
+    print("kullanici adinizi kontrol edin")
+
+
+
+
